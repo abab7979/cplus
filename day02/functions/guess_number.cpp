dd36bb5ad98c1bcaaf2ff86c99ef23910050103c
abab7979 <abab7979@naver.com>
2021-11-13 13:26:53 +0900

    day02

---
+++ day02/functions/guess_number.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+
+int main() {
+
+	int i, com, guess, min = 1, max = 100;
+
+	srand((unsigned)time(NULL));    //0~43억
+	com = rand() % 100 + 1;         //컴퓨터가 추측한 숫자
+	//int i = 1;
+
+	//while (true) {
+	    for(i = 0; i < 20; i++) {
+		cout << "[" << i + 1 << "회]" << min << "~" << max << "사이의 값 예측=>";
+		cin >> guess;
+
+		//조건처리
+		if (guess == com) {
+			cout << "정답입니다" << endl;
+			break;
+		}
+		else if (guess > com) {
+			cout << "너무커요" << endl;
+			max = guess;
+		}
+		else {
+			cout << "너무작아요" << endl;
+			min = guess;
+		}
+			
+	}
+	cout << "정답:" << com<<endl;
+	cout << "최종점수" << (10 - i) * 10 << endl;
+	return 0;
+}
--- day02/functions/time_ex.cpp
+++ day02/functions/time_ex.cpp
@@ -1,13 +1,18 @@
-#include <iostream>
+/*#include <iostream>
 #include <cmath>
+#include <Windows.h>
 using namespace std;
 
 int main() {
-	long now;
-	now = time(NULL);
+	long start, end;
+	start = time(NULL);   //시작시간
+	//cout << now << "초" << endl;
 
-
-	cout << now << "초" << endl;
-
+	for (int i = 0; i < 100; i++) {
+		cout << i << endl;
+		Sleep(10);  //0.01초
+	}
+	end = time(NULL);
+	cout << "for문 수행 시간"<< (end-start) << "초" << endl;
 	return 0;
-}
+}*/
